ce84b9abe8d422b2ba5616c3b758f984261e2198
James Stevenson <James.L.Stevenson@gmail.com>
2024-09-16 17:12:00 +0100

    Missing loading json

--- WargamesAI/coordination/umpire.py
+++ WargamesAI/coordination/umpire.py
@@ -372,7 +372,7 @@ class Umpire:
         rules = self._game._game_rules_text
         players = self._game._teams
         prompt = self.llm.generate_json_prompt(json_schemas.DefaultModel, f"Based on the below game actions and the following game rules and players, summarise the gameplay that took effect. \n\n Actions: \n {actions}. \n \n Rules: {rules} \n \n Players: \n {players}")
-        response = self.llm.ask_question(prompt)
+        response = self.llm.ask_question(prompt)["RESPONSE"]
 
         return response
     
@@ -381,7 +381,7 @@ class Umpire:
         rules = self._game._game_rules_text
         players = self._game._teams
         prompt = self.llm.generate_json_prompt(json_schemas.WinModel, f"Based on the below game actions and the following game rules and players, Deduce the winner of the gameplay that took effect. \n\n Actions: \n {actions}. \n \n Rules: {rules} \n \n Players: \n {players}")
-        response = self.llm.ask_question(prompt)
+        response = self.llm.ask_question(prompt)["RESPONSE"]
 
         return response
     
