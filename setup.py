e400c3d552bffd03287d5c61eecbb9ca760ea015
James Stevenson <James.L.Stevenson@gmail.com>
2024-09-16 22:30:51 +0100

    tuning prompt

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name="WargamesAI",  # Name of the tool/package
-    version="0.4.8",  # Current version
+    version="0.4.9",  # Current version
     url="https://github.com/user1342/WargamesAI",
     packages=find_packages(),  # Automatically find package directories
     classifiers=[
